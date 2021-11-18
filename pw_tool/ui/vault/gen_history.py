dd6f95f585cd85e4047c5faddf860206072c6811
xwlim <xwlim1611@gmail.com>
2021-11-18 23:09:54 +0800

    updated gen hist

--- pw_tool/ui/vault/gen_history.py
+++ pw_tool/ui/vault/gen_history.py
@@ -2,6 +2,7 @@ import tkinter.ttk
 
 import pw_tool.helper.ui_helper
 import pw_tool.helper.vault_helper
+import pw_tool.ui.vault.clear_dialog
 
 class GenHistPage:
     def __init__(self, master):
@@ -27,5 +28,5 @@ class GenHistPage:
 
     def __show_clear_dialog(self):
         self.__window.destroy()
-        pw_tool.ui.vault.clear_dialog.ClearDialog(master=self.__master, website=self.__website)
+        pw_tool.ui.vault.clear_dialog.ClearDialog(master=self.__master)
 
